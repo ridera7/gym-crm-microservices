7c16570e8ad3edee5f03a7ed5b20352f1f0d01a7
ridera7 <ridera7@gmail.com>
2024-11-30 21:45:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Gym-Crm-Microservices
---
+++ service-working-hours/src/main/java/com/service/working/hours/ServiceWorkingHoursApplication.java
@@ -0,0 +1,13 @@
+package com.service.working.hours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceWorkingHoursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceWorkingHoursApplication.class, args);
+    }
+
+}
